28571167b12f576e3e465fb31707589ac0963cc6
Altangerel <aagii4520@gmail.com>
2023-07-18 10:49:15 +0300

    complete add user function

--- src/main/java/com/example/QuizApp/Controller/UserController.java
+++ src/main/java/com/example/QuizApp/Controller/UserController.java
@@ -8,22 +8,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/user");
-public class UserController {
+import java.security.PublicKey;
 
+@RestController
+@RequestMapping("/user")
+public class UserController {
     @Autowired
     private UserService userService;
 
     @PostMapping("/add")
     public String add(@RequestBody User user) {
         userService.saveUser(user);
-        return "New user is added";
+        return "New User added.";
     }
+
 }
-
-
-
-
-
-
--- src/main/java/com/example/QuizApp/Service/UserService.java
+++ src/main/java/com/example/QuizApp/Service/UserService.java
@@ -2,14 +2,13 @@ package com.example.QuizApp.Service;
 
 import com.example.QuizApp.Model.User;
 
-import java.util.List;
 
 public interface UserService {
 
-    User saveUser(User user);
-    User getUserById(int id);
-    User UpdateUser(User user);
-    void deleteUserById (int id);
+    public User saveUser(User user);
+//    User getUserById(int id);
+//    User UpdateUser(User user);
+//    void deleteUserById (int id);
 
 
 }
